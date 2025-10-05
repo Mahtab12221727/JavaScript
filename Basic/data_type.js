565cd06c2778a64821ca5f56b5c3c49b7d464668
Mahtab sheikh <119559082+Mahtab12221727@users.noreply.github.com>
2025-10-05 07:34:21 +0000

    discussion of datatype

---
+++ Basic/data_type.js
@@ -0,0 +1,38 @@
+/*primitive stack type memory
+string 
+number
+Bolean
+NUll
+undefined
+symbole
+BigInt
+*/
+const firstname="alam"
+const age=20;
+const score=200
+let isLoggin=true
+const mnodeVersion=22.02
+let bankBlance=undefined
+const buttonId=Symbol("id")
+const reallyLongNumber=7324945487n
+
+
+//Refrence(non-primitive) heap typer memory
+/*
+Arrays
+Objext
+function
+*/
+const numbeer=[1,2,3,4];
+const heros=["salman","akshay","ali"]
+const userObject={
+   name:"alam",
+   age:23,
+   isLoggin:true
+}
+const sayHello=function(){
+    console.log("sayHello")
+}
+const registerUser=function(){
+
+}
